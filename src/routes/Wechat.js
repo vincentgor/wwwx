c5f08ab50beffa5c2a848f07accbfca821b6fe9e
vincent_gor <1149536116@qq.com>
2016-06-27 16:29:28 +0000

    增加微信登录功能

--- src/routes/Wechat.js
+++ src/routes/Wechat.js
@@ -15,9 +15,11 @@ module.exports = function (router) {
     // 验证登录
     router.get('/login', function* (next) {
         if (!this.session.code) {
+            let url = encodeURIComponent('https://wwwx-vincent-gor.c9users.io/api/v1/wechat/accesstoken');
+            console.log(url);
             this.body = {
                 code: 1,
-                url: `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx645b499c4950a4c3&redirect_uri=REDIRECT_URI&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect`
+                url: `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx645b499c4950a4c3&redirect_uri=` + url + `&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect`
             };
             return;
         } else {
@@ -28,16 +30,31 @@ module.exports = function (router) {
         }
     });
 
+    let getFans = function(access_token, openid, lang ) {
+        lang = lang || 'zh_CN';
+        let url = 'https://api.weixin.qq.com/sns/userinfo?access_token={access_token}&openid={openid}&lang={lang}';
+        url = url.replace('{access_token}', access_token);
+        url = url.replace('{openid}', openid);
+        url = url.replace('{lang}', lang);
+        console.log('url', url);
+        return requestGet(url).then((result) => {
+            console.log(result.body);
+            return result.body;
+        });
+    };
+
     // 回调（请求参数 code， 进而获取 access_token）
     router.get('/accesstoken', function* (next) {
         let code = this.query.code;
         var url = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx645b499c4950a4c3&secret=8ed61397d934d4af3bcb4419acf3e83c&code=`+ code + `&grant_type=authorization_code`;
         console.log('url', url);
         let result = yield requestGet(url).then((result) => {
-            console.log(result.body);
-            return result;
+            
+            return JSON.parse(result.body);
         });
-        this.body = result.body;
+        console.log('result', typeof result);
+        let lalala = yield getFans(result.access_token, result.openid);
+        this.body = lalala;
     });
 
 };
